b85e95b8bd0672a672a868132ea9fa917107b4f3
Lfairf <lukefairfax@gmail.com>
2022-11-17 01:50:01 +0000

    Character Rotation + minor tweaks
    Lighting and character position changed slightly

---
+++ Assets/Scripts/DragRotate.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragRotate : MonoBehaviour
+{
+    public float rotationSpeed = 1f;
+
+    float sceneWidth;
+    Vector3 clickPoint;
+    Quaternion startRotation;
+
+    private void Start()
+    {
+        sceneWidth = Screen.width;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            clickPoint = Input.mousePosition;
+            startRotation = transform.rotation;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            float distBetween = (Input.mousePosition - clickPoint).x;
+            transform.rotation = startRotation * Quaternion.Euler(Vector3.down * (distBetween / sceneWidth) * 360);
+        }
+    }
+
+    /*void OnMouseDrag()
+    {
+        xRotation = Input.GetAxis("Mouse X") * rotationSpeed;
+        yRotation = Input.GetAxis("Mouse Y") * rotationSpeed;
+
+        transform.Rotate(Vector3.down, xRotation);
+        transform.Rotate(Vector3.right, yRotation);
+    }
+    */
+}
